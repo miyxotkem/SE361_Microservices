5fac1b42f26a4cbc5b87a010dbb8e92508482676
PeterBrr <dinhquangnhat16062006@gmail.com>
2026-05-05 14:29:40 +0700

    add teacher's schedule

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using e_learning_app.Views;
+using e_learning_app.Views;
 using Google.Cloud.Firestore;
 using System;
 using System.Windows;
@@ -36,6 +36,11 @@ namespace e_learning_app
         private void NavDashboard_Click(object sender, RoutedEventArgs e)
         {
             MainContentArea.Content = new TeacherDashboardView(_dbManager);
+        }
+
+        private void NavSchedule_Click(object sender, RoutedEventArgs e)
+        {
+            MainContentArea.Content = new TeachingScheduleView();
         }
 
         private void NavClasses_Click(object sender, RoutedEventArgs e)
